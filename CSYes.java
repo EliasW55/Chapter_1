9698d91cbea1f6c02e4fdbc943fe76ad3c550feb
EliasW55 <31353324+EliasW55@users.noreply.github.com>
2017-10-06 09:55:50 -0700

    Chapter 2 Java

---
+++ CSYes.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class CSYes here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */// *******************************************************************
+// File Name:
+//
+// Purpose:
+// *******************************************************************
+ 
+public class CSYes
+{
+    public static void main(String args[]) {
+        System.out.println("Computer Science !!!!");
+    }
+}
---
+++ Count.java
@@ -0,0 +1,19 @@
+
+/**
+ * Elias Warres - Count
+ * This program prints the first five numbers in
+ * the languages English, French, and Spanish
+ */
+public class Count
+{
+ 
+        public static void main (String[] args)
+        {
+ 
+          /*English*/System.out.println ("one two three four five");
+          /*French*/System.out.println ("un deux trois quatre cinq");
+          /*Spanish*/System.out.println ("uno dos tres cuatro cinco");
+ 
+        }
+ 
+}
